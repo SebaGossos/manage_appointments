43fd1f1196e3477b5bce54b483b0da8a500c8fa2
Sebastian <winigossos@gmail.com>
2025-02-26 19:56:34 -0300

    showing appointments

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ const dateInput = document.getElementById('fecha');
 const symptomsInput = document.getElementById('sintomas');
 const form = document.getElementById('formulario-cita');
 
+const appointmentContainer = document.querySelector('#citas')
+
 // events
 patientInput.addEventListener('change', dataAppointment);
 propertyInput.addEventListener('change', dataAppointment);
@@ -22,24 +24,6 @@ const appointmentObj = {
     email: '',
     date: '',
     symptoms: ''
-}
-
-// functions
-function dataAppointment( e ) {
-    appointmentObj[e.target.name] = e.target.value;
-}
-function submitAppointment( e ) {
-    e.preventDefault();
-    
-    const { patient, property, email, date, symptoms } = appointmentObj;
-    const areEmpty = Object.values(appointmentObj).some( value => !value?.trim());
-    if( areEmpty ){
-        new Notify({
-            text: 'all fields are required',
-            type: 'error'
-        })
-        return;
-    }
 }
 
 //class
@@ -83,3 +67,59 @@ class Notify {
 
     }
 }
+
+class AdminAppointments {
+    constructor(){
+        this.appointments = [];
+    };
+
+    add(appointment) {
+        this.appointments.push( appointment );
+        this.show();
+    }
+
+    show() {
+        // clean before HTML
+        while( appointmentContainer.firstChild ) {
+            appointmentContainer.removeChild(appointmentContainer.firstChild);
+        }
+        // generate appointments
+        this.appointments.forEach( appoint => {
+            const appointmentDiv = document.createElement('div');
+            appointmentDiv.classList.add('mx-5', 'my-10', 'bg-white', 'shadow-md', 'px-5', 'py-10', 'rounded-xl')
+
+            const patient = document.createElement('p');
+            patient.classList.add('font-normal', 'mb-3', 'text-gray-700')
+            patient.innerHTML = `<span class="font-bold uppercase">Paciente: </span> ${appoint.patient}`
+
+            // inject HTML
+            appointmentDiv.appendChild( patient );
+
+            appointmentContainer.appendChild(appointmentDiv)
+        })
+    }
+
+}
+
+
+// functions
+function dataAppointment( e ) {
+    appointmentObj[e.target.name] = e.target.value;
+}
+const appointments = new AdminAppointments()
+function submitAppointment( e ) {
+    e.preventDefault();
+    
+    const { patient, property, email, date, symptoms } = appointmentObj;
+    const areEmpty = Object.values(appointmentObj).some( value => !value?.trim());
+    if( areEmpty ){
+        new Notify({
+            text: 'all fields are required',
+            type: 'error'
+        })
+        return;
+    }
+
+    appointments.add(appointmentObj)
+}
+appointments.show()
